efcd2e3bb7a681596369618951a33e8e0a0b0af7
jd9913 <67403842+jd9913@users.noreply.github.com>
2020-12-30 10:39:05 -0700

    add mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -38,7 +38,12 @@ type Query{
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
+
+
   type Auth{
       token: ID!
       user: User
