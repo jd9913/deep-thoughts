398c8784a3d804e111d832ea56a3e9e11886ada7
jd9913 <67403842+jd9913@users.noreply.github.com>
2020-12-30 11:05:23 -0700

    updates to use Apollo connection to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+const client=new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
@@ -14,6 +21,7 @@ function App() {
       </div>
       <Footer />
     </div>
+    </ApolloProvider>
   );
 }
 
